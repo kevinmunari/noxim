eab1692f4484084c99c087992e450b9c5ac65115
kevinmunari <46374500+kevinmunari@users.noreply.github.com>
2019-03-03 18:02:49 +0100

    Added new params needed by Noxim to draw the graph

--- src/GraphViz.h
+++ src/GraphViz.h
@@ -17,10 +17,25 @@
 
 using namespace std;
 
+struct edge_NSWE
+{
+    Agedge_t* east;
+    Agedge_t* west;
+    Agedge_t* south;
+    Agedge_t* north;
+};
+
+// Declaration of graph attributes' variables
+char attribute_name[16];
+char attribute_value[30];
+char attribute_default_value[16];
+
 GVC_t *graphviz_context;
 Agraph_t *graph;
 map<int, Agnode_t*> channel_node;
 map<int, Agnode_t*> hub_node;
 map<int, Agnode_t*> tile_node;
+map<int, edge_NSWE> tile_tile_rx_edge;
+map<int, edge_NSWE> tile_tile_tx_edge;
 
 #endif
